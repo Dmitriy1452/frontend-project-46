5c909f4d3f180f56025e2cc14acedd7a319d70d9
Admin <tolstyachokhenkov@yandex.ru>
2025-10-06 23:38:48 +0300

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,44 @@
 import * as fs from 'node:fs'
 import { cwd } from 'node:process'
 import path from 'node:path'
+import _ from 'lodash'
 
-export const file = (filepath1, filepath2) => {
+const parseFiles = (filepath1, filepath2) => {
     const readFile1 = JSON.parse(fs.readFileSync(path.resolve(cwd(), filepath1)).toString())
     const readFile2 = JSON.parse(fs.readFileSync(path.resolve(cwd(), filepath2)).toString())
-    return [readFile1, readFile2]
+    const result = [readFile1, readFile2]
+    return result
+}
+
+const getSortedKeys = (files) => {
+    const keys = files.flatMap(file => Object.keys(file))
+    const uniqKeys = _.uniq(keys)
+    const sortedKeys = _.sortBy(uniqKeys)
+    return sortedKeys
+}
+
+export const genDiff = (filepath1, filepath2) => {
+    const [obj1, obj2] = parseFiles(filepath1, filepath2) 
+    const allKeys = getSortedKeys([obj1, obj2])
+
+    const result = allKeys.flatMap(key => {
+        const inObj1 = key in obj1
+        const inObj2 = key in obj2 
+
+        if (inObj1 && inObj2) {
+            if (obj1[key] === obj2[key]) {
+                return [`  ${key}: ${obj1[key]}`]
+            } else {
+                return [
+                    `- ${key}: ${obj1[key]}`,
+                    `+ ${key}: ${obj2[key]}`
+                ]
+            }
+        } else if (inObj1) {
+            return [`- ${key}: ${obj1[key]}`]
+        } else {
+            return [`+ ${key}: ${obj2[key]}`]
+        }
+    })
+    return `{\n${result.join('\n')}\n}`
 }
