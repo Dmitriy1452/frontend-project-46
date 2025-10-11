67b2b66c5b789abdf345064dcaf592deb5997130
Admin <tolstyachokhenkov@yandex.ru>
2025-10-11 18:07:38 +0300

    fixing linter errors

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -4,9 +4,9 @@ import { genDiff } from '../src/index.js'
 const program = new Command()
 
 program
-    .description('Compares two configuration files and shows a difference')
-    .option('-V, --version', 'output the version number')
-    .option('-f, --format [type]', 'output format')
-    .arguments('<filepath1> <filepath2>')
-    .action((filepath1, filepath2) => console.log(genDiff(filepath1, filepath2)))
+  .description('Compares two configuration files and shows a difference')
+  .option('-V, --version', 'output the version number')
+  .option('-f, --format [type]', 'output format')
+  .arguments('<filepath1> <filepath2>')
+  .action((filepath1, filepath2) => console.log(genDiff(filepath1, filepath2)))
 program.parse(process.argv)
--- src/index.js
+++ src/index.js
@@ -4,41 +4,44 @@ import path from 'node:path'
 import _ from 'lodash'
 
 const parseFiles = (filepath1, filepath2) => {
-    const readFile1 = JSON.parse(fs.readFileSync(path.resolve(cwd(), filepath1)).toString())
-    const readFile2 = JSON.parse(fs.readFileSync(path.resolve(cwd(), filepath2)).toString())
-    const result = [readFile1, readFile2]
-    return result
+  const readFile1 = JSON.parse(fs.readFileSync(path.resolve(cwd(), filepath1)).toString())
+  const readFile2 = JSON.parse(fs.readFileSync(path.resolve(cwd(), filepath2)).toString())
+  const result = [readFile1, readFile2]
+  return result
 }
 
 const getSortedKeys = (files) => {
-    const keys = files.flatMap(file => Object.keys(file))
-    const uniqKeys = _.uniq(keys)
-    const sortedKeys = _.sortBy(uniqKeys)
-    return sortedKeys
+  const keys = files.flatMap(file => Object.keys(file))
+  const uniqKeys = _.uniq(keys)
+  const sortedKeys = _.sortBy(uniqKeys)
+  return sortedKeys
 }
 
 export const genDiff = (filepath1, filepath2) => {
-    const [obj1, obj2] = parseFiles(filepath1, filepath2) 
-    const allKeys = getSortedKeys([obj1, obj2])
+  const [obj1, obj2] = parseFiles(filepath1, filepath2)
+  const allKeys = getSortedKeys([obj1, obj2])
 
-    const result = allKeys.flatMap(key => {
-        const inObj1 = key in obj1
-        const inObj2 = key in obj2 
+  const result = allKeys.flatMap((key) => {
+    const inObj1 = key in obj1
+    const inObj2 = key in obj2
 
-        if (inObj1 && inObj2) {
-            if (obj1[key] === obj2[key]) {
-                return [`  ${key}: ${obj1[key]}`]
-            } else {
-                return [
-                    `- ${key}: ${obj1[key]}`,
-                    `+ ${key}: ${obj2[key]}`
-                ]
-            }
-        } else if (inObj1) {
-            return [`- ${key}: ${obj1[key]}`]
-        } else {
-            return [`+ ${key}: ${obj2[key]}`]
-        }
-    })
-    return `{\n${result.join('\n')}\n}`
+    if (inObj1 && inObj2) {
+      if (obj1[key] === obj2[key]) {
+        return [`  ${key}: ${obj1[key]}`]
+      }
+      else {
+        return [
+          `- ${key}: ${obj1[key]}`,
+          `+ ${key}: ${obj2[key]}`,
+        ]
+      }
+    }
+    else if (inObj1) {
+      return [`- ${key}: ${obj1[key]}`]
+    }
+    else {
+      return [`+ ${key}: ${obj2[key]}`]
+    }
+  })
+  return `{\n${result.join('\n')}\n}`
 }
