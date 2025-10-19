a635fb9ed16455b443383529ea034765db300dee
Admin <tolstyachokhenkov@yandex.ru>
2025-10-19 16:00:13 +0300

    add new test wich check comparing YAML files

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,5 +1,5 @@
 import { genDiff } from '../src/index.js'
-import { test, expect } from '@jest/globals'
+import { describe, test, expect } from '@jest/globals'
 import { fileURLToPath } from 'node:url'
 import { dirname } from 'node:path'
 import path from 'node:path'
@@ -11,11 +11,31 @@ const __dirname = dirname(__filename)
 const getFixturesPath = filename => path.join(__dirname, '..', '__fixtures__', filename)
 const readFile = filename => fs.readFileSync(getFixturesPath(filename), 'utf-8')
 
-test('Comparing two files', () => {
+test('Comparing JSON files', () => {
   const file1 = getFixturesPath('file1.json')
   const file2 = getFixturesPath('file2.json')
   const expected = readFile('expected-result.txt')
   const result = genDiff(file1, file2)
 
   expect(result).toEqual(expected)
+})
+
+describe('Compare YAML files', () => {
+  test('Compare with ".yml" extension', () => {
+    const file1 = getFixturesPath('file1.yml')
+    const file2 = getFixturesPath('file2.yml')
+    const expected = readFile('expected-reuslt.txt')
+    const result = genDiff(file1, file2)
+
+    expect(result).toEqual(expected)
+  })
+
+  test('Compare with ".yaml" extension', () => {
+    const file1 = getFixturesPath('file1.yaml')
+    const file2 = getFixturesPath('file2.yaml')
+    const expected = readFile('expected-result.txt')
+    const result = genDiff(file1, file2)
+
+    expect(result).toEqual(expected)
+  })
 })
