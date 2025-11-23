37ca66392aba723ad42964402ff8fdbf5f8b097d
Admin <tolstyachokhenkov@yandex.ru>
2025-11-23 16:06:27 +0300

    added the default formatter

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,7 +6,7 @@ const program = new Command()
 program
   .description('Compares two configuration files and shows a difference')
   .version('1.0.0', '-V, --version', 'output the current version')
-  .option('-f, --format [type]', 'output format')
+  .option('-f, --format [type]', 'output format', 'stylish')
   .arguments('<filepath1> <filepath2>')
   .action((filepath1, filepath2) => console.log(genDiff(filepath1, filepath2)))
 program.parse(process.argv)
