cb4db458b99b9219e430dc18dd1e36d2c948e6f8
Admin <tolstyachokhenkov@yandex.ru>
2025-11-23 17:19:19 +0300

    add new tests and refactoring

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -11,31 +11,47 @@ const __dirname = dirname(__filename)
 const getFixturesPath = filename => path.join(__dirname, '..', '__fixtures__', filename)
 const readFile = filename => fs.readFileSync(getFixturesPath(filename), 'utf-8')
 
-test('Comparing JSON files', () => {
+const expectedStylish = readFile('expected-result.txt')
+const expectedPlain = readFile('expected-plain.txt')
+
+describe('Formatters', () => {
   const file1 = getFixturesPath('second-file1.json')
   const file2 = getFixturesPath('second-file2.json')
-  const expected = readFile('expected-result.txt')
-  const result = genDiff(file1, file2)
 
-  expect(result).toEqual(expected)
-})
+  describe('stylish format', () => {
+    test('should format diff in stylish style', () => {
+      const result = genDiff(file1, file2, 'stylish')
+      expect(result).toEqual(expectedStylish)
+    })
 
-describe('Compare YAML files', () => {
-  test('Compare with ".yml" extension', () => {
-    const file1 = getFixturesPath('second-file1.yml')
-    const file2 = getFixturesPath('second-file2.yml')
-    const expected = readFile('expected-result.txt')
-    const result = genDiff(file1, file2)
-
-    expect(result).toEqual(expected)
+    test('stylish should be default format', () => {
+      const result = genDiff(file1, file2)
+      expect(result).toEqual(expectedStylish)
+    })
   })
 
-  test('Compare with ".yaml" extension', () => {
-    const file1 = getFixturesPath('second-file1.yaml')
-    const file2 = getFixturesPath('second-file2.yaml')
-    const expected = readFile('expected-result.txt')
-    const result = genDiff(file1, file2)
+  describe('plain format', () => {
+    test('should format diff in plain style', () => {
+      const result = genDiff(file1, file2, 'plain')
+      expect(result).toEqual(expectedPlain)
+    })
 
-    expect(result).toEqual(expected)
+    test('should handle complex values', () => {
+      const result = genDiff(file1, file2, 'plain')
+
+      expect(result).toContain('was added with value: [complex value]')
+      expect(result).toContain('was updated. From [complex value] to \'str\'')
+    })
+
+    test('should handle nested properties', () => {
+      const result = genDiff(file1, file2, 'plain')
+
+      expect(result).toContain('\'common.setting6.doge.wow\'')
+      expect(result).toContain('\'common.setting6.ops\'')
+    })
+  })
+
+  test('should throw error for unknown format', () => {
+    expect(() => genDiff(file1, file2, 'unknown')).toThrow('Unknown format: unknown')
   })
 })
